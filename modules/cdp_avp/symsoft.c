9eadd7a2b13ee59dde045a6723429ba9740c512f
Pablo Ruiz <pruiz@tuenti.com>
2014-09-16 11:33:03 +0200

    Fixed license header

--- modules/cdp_avp/symsoft.c
+++ modules/cdp_avp/symsoft.c
@@ -1,26 +1,9 @@
 /*
  * $Id$
  *
- * The initial version of this code was written by Dragos Vingarzan
- * (dragos(dot)vingarzan(at)fokus(dot)fraunhofer(dot)de and the
- * Fruanhofer Institute. It was and still is maintained in a separate
- * branch of the original SER. We are therefore migrating it to
- * Kamailio/SR and look forward to maintaining it from here on out.
- * 2011/2012 Smile Communications, Pty. Ltd.
- * ported/maintained/improved by 
- * Jason Penton (jason(dot)penton(at)smilecoms.com and
- * Richard Good (richard(dot)good(at)smilecoms.com) as part of an 
- * effort to add full IMS support to Kamailio/SR using a new and
- * improved architecture
- * 
- * NB: Alot of this code was originally part of OpenIMSCore,
- * FhG Focus. Thanks for great work! This is an effort to 
- * break apart the various CSCF functions into logically separate
- * components. We hope this will drive wider use. We also feel
- * that in this way the architecture is more complete and thereby easier
- * to manage in the Kamailio/SR environment
- *
- * This file is part of Kamailio, a free SIP server.
+ * SYMSOFT OCS Diameter Support AVPs.
+ * (C) 2014 Pablo Ruiz García <pruiz@netway.org>
+ * Based on existing C-Diameter code from OpenIMS
  *
  * Kamailio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
